9d0b8d2648d752230b58b38ad21e353c0b270cc0
Michelle Lamoureaux <michellelamx@macbook.local>
2023-05-25 15:00:24 -0700

    removing .nextjs folder

--- .next/server/next-font-manifest.js
+++
@@ -1,1 +0,0 @@
-self.__NEXT_FONT_MANIFEST={pages:{},app:{'/Users/michellelamx/repos/nextjs-display-api-data/node_modules/next/font/google/target.css?{"path":"app/layout.js","import":"Montserrat","arguments":[{"subsets":["latin"]}],"variableName":"montserrat"}':["static/media/1060bab20f18b5c2-s.p.woff2"]},appUsingSizeAdjust:!0,pagesUsingSizeAdjust:!1};
